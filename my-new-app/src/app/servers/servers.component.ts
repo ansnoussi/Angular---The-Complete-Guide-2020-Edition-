75694cc35778d37f2c0839f2294b73d62d039e85
AnSnoussi <snoussi.anis@insat.u-carthage.tn>
2020-06-16 07:11:01 +0100

    event + data binding

--- my-new-app/src/app/servers/servers.component.ts
+++ my-new-app/src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServers = false;
   serverCreationStatus = 'No server was created';
+  serverName = '';
 
   constructor() {
     setTimeout(() => {
@@ -19,5 +20,11 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server was created';
+  }
+
+  onUpdateServerName(event: any) {
+    // we can explicitly inform typescript about the type like so:
+    // this.serverName = (<HTMLInputElement>event.target).value;
+    this.serverName = event.target.value;
   }
 }
